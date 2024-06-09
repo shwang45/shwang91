19e76594df50030f232892f31700eec032c1e698
song <thdtn63@hanmail.net>
2024-06-09 22:24:41 +0900

    next commit

---
+++ dr-bread/postcss.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  output: "export",
+  reactStrictMode: true,
+  plugins: {
+    tailwindcss: {},
+  },
+};
+
+export default config;

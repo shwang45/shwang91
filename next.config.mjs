aed9401d3c940ddcc4a3b1f77a43013c6c5c4cc2
song <thdtn63@hanmail.net>
2024-06-09 22:42:13 +0900

    basePath 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/dr.bread",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+const BASE_PATH = process.env.BASE_PATH || "";
+export { BASE_PATH };

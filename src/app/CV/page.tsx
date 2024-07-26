0edae7f1559f2004d2c390f50d79432444acf279
shwang45 <102244337+shwang45@users.noreply.github.com>
2024-07-26 16:59:40 -0700

    Update page.tsx

--- src/app/CV/page.tsx
+++ src/app/CV/page.tsx
@@ -3,7 +3,7 @@ export default function () {
   return (
     <div>
       <p>
-        <iframe src="https://drive.google.com/file/d/1mSk_W9zWS9jLHnDnXcWxWDhZENIcgSIx/preview" width="900" height="1000" title="Google Drive File"></iframe>
+        <iframe src="https://drive.google.com/file/d/1TB3mMYzUuGQdlWk3DV_aBdp5xO3hiCLb/preview" width="900" height="1000" title="Google Drive File"></iframe>
       </p>
     </div>
 
